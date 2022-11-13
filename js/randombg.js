6324e848ea335909bdaaf63049adac4612af8623
gaoziman <2942894660@qq.com>
2022-11-13 22:55:31 +0800

    Site updated: 2022-11-13 22:55:28

--- js/randombg.js
+++ js/randombg.js
@@ -1,9 +1,10 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
     "url(https://yzf.qq.com/fsna/kf-file/kf_pic/20221016/KFPIC_56149c9206ac38555_WXIMAGE_00bff74eafb94f6a9d68e7fec85ef149.jpg?)",
-    "url(https://source.fomal.cc/img/default_cover_1.webp)",
+    "url(https://source.fomal.cc/img/default_cover_160.webp)",
     "url(https://source.fomal.cc/img/default_cover_2.webp)",
-    "url(https://source.fomal.cc/img/default_cover_92.webp)"
+    "url(http://img.hb.aicdn.com/a13644264e8d4f5f52a74c88d75d8ff22c34eb12f6b4c5-k1IIl6)",
+    "url(https://source.fomal.cc/img/dm5.webp)"
   ];
   //获取背景图片总数，生成随机数
   var bgindex =Math.floor(Math.random() * backimg.length);
@@ -12,9 +13,10 @@ var backimg =[
   //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
   var bannerimg =[
     "url(https://yzf.qq.com/fsna/kf-file/kf_pic/20221016/KFPIC_56149c9206ac38555_WXIMAGE_00bff74eafb94f6a9d68e7fec85ef149.jpg?)",
-    "url(https://source.fomal.cc/img/default_cover_1.webp)",
+    "url(https://source.fomal.cc/img/default_cover_160.webp)",
     "url(https://source.fomal.cc/img/default_cover_2.webp)",
-    "url(https://source.fomal.cc/img/default_cover_92.webp)"
+    "url(http://img.hb.aicdn.com/a13644264e8d4f5f52a74c88d75d8ff22c34eb12f6b4c5-k1IIl6)",
+    "url(https://source.fomal.cc/img/dm5.webp)"
   ];
   //获取banner图片总数，生成随机数
   var bannerindex =Math.floor(Math.random() * bannerimg.length);
