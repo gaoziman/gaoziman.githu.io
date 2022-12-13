bb2d6828f484caedafa5514c2a43e5bc29b437be
gaoziman <2942894660@qq.com>
2022-12-13 21:37:31 +0800

    Site updated: 2022-12-13 21:37:27

--- js/randombg.js
+++ js/randombg.js
@@ -8,13 +8,13 @@ var backimg =[
   //获取背景图片总数，生成随机数
   var bgindex =Math.floor(Math.random() * backimg.length);
   //重设背景图片
-  document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
-  //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
-  var bannerimg =[
-    "url(https://gaoziman.oss-cn-hangzhou.aliyuncs.com/img/t01dc4e3032b4e0ec4b.jpg)",
-    "url(https://gaoziman.oss-cn-hangzhou.aliyuncs.com/img/Sitting%20on%20an%20Elk.png)",
-    "url(https://gaoziman.oss-cn-hangzhou.aliyuncs.com/img/dawn%20of%20war.png)",
-    "url(https://gaoziman.oss-cn-hangzhou.aliyuncs.com/img/chaos%20invoked.png)"
+    document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
+    //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
+    var bannerimg =[
+      "url(https://gaoziman.oss-cn-hangzhou.aliyuncs.com/img/t01dc4e3032b4e0ec4b.jpg)",
+      "url(https://gaoziman.oss-cn-hangzhou.aliyuncs.com/img/Sitting%20on%20an%20Elk.png)",
+      "url(https://gaoziman.oss-cn-hangzhou.aliyuncs.com/img/dawn%20of%20war.png)",
+      "url(https://gaoziman.oss-cn-hangzhou.aliyuncs.com/img/chaos%20invoked.png)"
   ];
   //获取banner图片总数，生成随机数
   var bannerindex =Math.floor(Math.random() * bannerimg.length);
